79ce941501a68781818e6cd51440c04a8a193d5d
quesitorey <galleta_chips@hotmail.com>
2023-05-13 17:26:33 -0500

    galletita

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
-import phrases from '/phrases.json'
+import phrases from '/src/phrases.json'
 function App() {
 
   const [ index, setIndex ] = useState(0)
   const [ phrase, setPhrase ] = useState([])
   const images = [ "/imagenes/fondo1.jpg", "/imagenes/fondo2.jpg", "/imagenes/fondo3.jpg", "/imagenes/fondo4.jpg" ]
 
-  const phrasess = "/phrases.json"
+  const phrasess = "/src/phrases.json"
   let data;
   const randomNum = () => Math.floor(Math.random() * data.length ) + 1;
 
